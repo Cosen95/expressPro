11136c42bbe82e8236ca1c9593ce4a4310538d29
fengshuan <1263215592@qq.com>
2018-11-23 23:14:20 +0800

    feat:新增express-session插件

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,7 +18,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+  secret: 'recommend 128 bytes random string', // 对session id 相关的cookie 进行签名
+  cookie: { maxAge: 20*60*1000 }, // // 设置 session 的有效时间，单位毫秒
+  resave: true,
+  saveUninitialized: true // 是否保存未初始化的会话
+}))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
